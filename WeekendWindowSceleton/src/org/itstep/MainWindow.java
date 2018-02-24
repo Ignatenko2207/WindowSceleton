2f1520a4237246f17c8013146985765b9fcb39f5
Alex <ignatenko2207@gmail.com>
2018-02-24 16:21:39 +0200

    Window sceleton

---
+++ WeekendWindowSceleton/src/org/itstep/MainWindow.java
@@ -0,0 +1,60 @@
+package org.itstep;
+
+import javax.swing.JFrame;
+import javax.swing.JTextPane;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class MainWindow extends JFrame{
+	private JTextField names;
+	private JTextField outField;
+
+	public MainWindow(String title) {
+		setTitle(title);
+		setSize(400, 140);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setVisible(true);
+		setLocation(200, 200);
+		
+		setResizable(false);
+		getContentPane().setLayout(null);
+		
+		JLabel lblFirsName = new JLabel("First and second names:");
+		lblFirsName.setBounds(10, 11, 122, 20);
+		getContentPane().add(lblFirsName);
+		
+		names = new JTextField();
+		names.setText("input first and second names");
+		names.setBounds(139, 11, 245, 20);
+		getContentPane().add(names);
+		names.setColumns(10);
+		
+		JButton btnShowFirsName = new JButton("REVERSE NAMES");
+		btnShowFirsName.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				outField.setText(reverseText(names.getText()));
+				names.setText("");
+				getContentPane().repaint();
+			}
+		});
+		btnShowFirsName.setBounds(10, 42, 374, 23);
+		getContentPane().add(btnShowFirsName);
+		
+		outField = new JTextField();
+		outField.setEditable(false);
+		outField.setBounds(10, 76, 374, 20);
+		getContentPane().add(outField);
+		outField.setColumns(10);
+		
+		getContentPane().repaint();
+		
+	}
+	
+	private String reverseText(String text) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
---
+++ WeekendWindowSceleton/src/org/itstep/WeekendWindowSceletonRunner.java
@@ -0,0 +1,8 @@
+package org.itstep;
+
+public class WeekendWindowSceletonRunner {
+
+	public static void main(String[] args) {
+		MainWindow window = new MainWindow("My window");
+	}
+}
